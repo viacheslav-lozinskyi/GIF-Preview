c48412edea97e5dbde373bf769670caad70a96ea
Viacheslav Lozinskyi <vl.spirit@gmail.com>
2020-08-02 11:30:45 +0300

    - Initial release

---
+++ application/preview-gif.vs/properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(resource.package.PreviewGIFPackage.CONSTANT.NAME)]
+[assembly: AssemblyDescription(resource.package.PreviewGIFPackage.CONSTANT.DESCRIPTION)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(resource.package.PreviewGIFPackage.CONSTANT.NAME)]
+[assembly: AssemblyCopyright(resource.package.PreviewGIFPackage.CONSTANT.COPYRIGHT)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(resource.package.PreviewGIFPackage.CONSTANT.VERSION)]
+[assembly: AssemblyFileVersion(resource.package.PreviewGIFPackage.CONSTANT.VERSION)]
---
+++ application/preview-gif.vs/resource/package/VSPackage.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace resource.package
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(CONSTANT.NAME, CONSTANT.DESCRIPTION, CONSTANT.VERSION)]
+    [Guid(CONSTANT.GUID)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.FirstLaunchSetup_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class PreviewGIFPackage : AsyncPackage
+    {
+        internal static class CONSTANT
+        {
+            public const string COPYRIGHT = "Copyright (c) 2020 by Viacheslav Lozinskyi. All rights reserved.";
+            public const string DESCRIPTION = "Quick preview of GIF files";
+            public const string EXTENSION = ".GIF";
+            public const string GUID = "B137E86C-D665-4EB6-A0B4-2907B8F24DF3";
+            public const string NAME = "Preview-GIF";
+            public const string VERSION = "1.0.1";
+        }
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            {
+                cartridge.AnyPreview.Connect();
+                cartridge.AnyPreview.Register(cartridge.AnyPreview.MODE.PREVIEW, CONSTANT.EXTENSION, new preview.GIF());
+            }
+        }
+
+        protected override int QueryClose(out bool canClose)
+        {
+            {
+                cartridge.AnyPreview.Disconnect();
+                canClose = true;
+            }
+            return VSConstants.S_OK;
+        }
+    }
+}
