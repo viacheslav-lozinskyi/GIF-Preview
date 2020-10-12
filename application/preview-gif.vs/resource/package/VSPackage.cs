e2524ced35299504e403ca7c3b98bd761f3d5115
Viacheslav Lozinskyi <vl.spirit@gmail.com>
2020-10-12 21:48:09 +0300

    - MetaPreview is updated to version 1.1.1 - Minor improvements

--- application/preview-gif.vs/resource/package/VSPackage.cs
+++ application/preview-gif.vs/resource/package/VSPackage.cs
@@ -11,11 +11,7 @@ namespace resource.package
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(CONSTANT.NAME, CONSTANT.DESCRIPTION, CONSTANT.VERSION)]
     [Guid(CONSTANT.GUID)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.FirstLaunchSetup_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class PreviewGIFPackage : AsyncPackage
     {
         internal static class CONSTANT
@@ -25,15 +21,17 @@ namespace resource.package
             public const string EXTENSION = ".GIF";
             public const string GUID = "B137E86C-D665-4EB6-A0B4-2907B8F24DF3";
             public const string NAME = "Preview-GIF";
-            public const string VERSION = "1.0.1";
+            public const string VERSION = "1.0.2";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             {
                 cartridge.AnyPreview.Connect();
                 cartridge.AnyPreview.Register(cartridge.AnyPreview.MODE.PREVIEW, CONSTANT.EXTENSION, new preview.GIF());
+            }
+            {
+                await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
             }
         }
 
